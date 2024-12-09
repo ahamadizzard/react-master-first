05144eebfd0c2c174020a807bdfed8411156e392
Ahamed Izzard <ahamadizzard@hotmail.com>
2024-12-10 00:14:09 +0530

    Started to build the login screen

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,7 +4,6 @@ export default function login() {
   return ( 
     <div className="flex flex-col justify-center items-center w-full min-h-screen bg-lime-200 text-black text-center"> 
       <div className="rounded-lg px-5 py-3 font-bold text-2xl bg-green-600"> 
-        
         Login Here
       </div>
     </div>
