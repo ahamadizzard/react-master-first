f9d09d342d0ca0a1d0d6f8470f2922c6273d2f8c
Ahamed Izzard <ahamadizzard@hotmail.com>
2024-12-14 23:37:24 +0530

    Login Screen Done

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,13 +1,10 @@
-"use client"; // This is a client component. It's rendered on the browser.
+"use client"; // This is a client component. It's rendered on the browser. this is specific to nextjs
+import LoginForm from "./login-form";
 
-export default function login() { 
-  return ( 
-    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-lime-200 text-black text-center"> 
-      <div className="rounded-lg px-5 py-3 font-bold text-2xl bg-green-600"> 
-        Login Here
-      </div>
+export default function login() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen py-2 text-black">
+      <LoginForm />
     </div>
-  ); 
+  );
 }
-
-
